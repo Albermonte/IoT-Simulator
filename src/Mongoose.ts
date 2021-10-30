d21b4d00b9f1cacea3780a77e72a0b59c7cda3f2
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2021-10-30 16:03:14 +0200

    localhost ip

--- src/Mongoose.ts
+++ src/Mongoose.ts
@@ -5,7 +5,7 @@ import Simulation from "./Simulation";
 export default class Mongoose {
   public static async connect() {
     try {
-      await mongoose.connect(`mongodb://root:${process.env.MONGO_ROOT_PASS}@mongodb:27017/telemetry?authSource=admin`);
+      await mongoose.connect(`mongodb://root:${process.env.MONGO_ROOT_PASS}@127.0.0.1:27017/telemetry?authSource=admin`);
       console.log("Connected to MongoDB");
       Simulation.start();
     } catch (error) {
